0c95d272d92f71ff2658bca072842504b9e44d95
xottabbu4 <113995327+xottabbu4@users.noreply.github.com>
2023-01-28 20:27:02 +0200

    added api and trending list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { GlobalStyleComponent } from 'styles/GlobalStyles';
-import { Layout } from '../components/Layout/Layout';
+import { Layout } from 'components/Layout/Layout';
 
 export const App = () => {
   return (
   <div>
     <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />} />
+        <Route index element={<Home />} />
         <Route path="/movies" element={<Movies />} />
       </Routes>
   </div>
