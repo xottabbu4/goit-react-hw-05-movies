5fa1db1e156ee82e6f3aaa719922e7a91f14a3ae
xottabbu4 <113995327+xottabbu4@users.noreply.github.com>
2023-01-28 20:13:11 +0200

    added components and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home/Home';
-import Movies from '../pages/Movies/Movies';
+import { GlobalStyleComponent } from 'styles/GlobalStyles';
+import { Layout } from '../components/Layout/Layout';
 
 export const App = () => {
   return (
-  <>
+  <div>
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
       </Routes>
-  </>
+  </div>
   );
 };
---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+  background-color: #d6822229;
+  min-height: 100vh;
+`;
